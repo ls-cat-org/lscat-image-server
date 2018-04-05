c0d5da6076d2a5cacff1b3914dc348e35256f441
Keith Brister <k-brister@northwestern.edu>
2018-04-05 08:22:09 -0500

    messed up the 2nd argument to va_start (but everything still worked...)

--- isLogging.c
+++ isLogging.c
@@ -4,12 +4,22 @@
  *  @copyright 2018 by Northwestern University. All rights reserved
  *  @author Keith Brister
  *
- *  Since we are going to be logging stuff from all kinds of different
- *  threads we'll set up a queue locked with a mutex.  When something
- *  has been added to the queue we'll know cause isLogging_cond will
- *  trigger us to read the queue and log the message with syslog.
- *
+ *  Gee, isn't it nice that syslog is already thread safe?
  */
+
+/*
+ * From the syslog man page
+ *
+ *      LOG_EMERG      system is unusable
+ *      LOG_ALERT      action must be taken immediately
+ *      LOG_CRIT       critical conditions
+ *      LOG_ERR        error conditions
+ *      LOG_WARNING    warning conditions
+ *      LOG_NOTICE     normal, but significant, condition
+ *      LOG_INFO       informational message
+ *      LOG_DEBUG      debug-level message
+ */
+
 #include "is.h"
 
 
@@ -17,29 +27,11 @@
 void isLogging_init() {
   openlog("is", LOG_PID | LOG_PERROR, LOG_USER);
 };
-/*
-
-       LOG_EMERG      system is unusable
-
-       LOG_ALERT      action must be taken immediately
-
-       LOG_CRIT       critical conditions
-
-       LOG_ERR        error conditions
-
-       LOG_WARNING    warning conditions
-
-       LOG_NOTICE     normal, but significant, condition
-
-       LOG_INFO       informational message
-
-       LOG_DEBUG      debug-level message
-*/
 
 void isLogging_debug(char *fmt, ...) {
   va_list arg_ptr;
 
-  va_start(arg_ptr, *fmt);
+  va_start(arg_ptr, fmt);
   vsyslog(LOG_DEBUG, fmt, arg_ptr);
   va_end(arg_ptr);
 }
@@ -47,7 +39,7 @@ void isLogging_debug(char *fmt, ...) {
 void isLogging_info(char *fmt, ...) {
   va_list arg_ptr;
 
-  va_start(arg_ptr, *fmt);
+  va_start(arg_ptr, fmt);
   vsyslog(LOG_INFO, fmt, arg_ptr);
   va_end(arg_ptr);
 }
@@ -55,7 +47,7 @@ void isLogging_info(char *fmt, ...) {
 void isLogging_notice(char *fmt, ...) {
   va_list arg_ptr;
 
-  va_start(arg_ptr, *fmt);
+  va_start(arg_ptr, fmt);
   vsyslog(LOG_NOTICE, fmt, arg_ptr);
   va_end(arg_ptr);
 }
@@ -63,7 +55,7 @@ void isLogging_notice(char *fmt, ...) {
 void isLogging_warning(char *fmt, ...) {
   va_list arg_ptr;
 
-  va_start(arg_ptr, *fmt);
+  va_start(arg_ptr, fmt);
   vsyslog(LOG_WARNING, fmt, arg_ptr);
   va_end(arg_ptr);
 }
@@ -71,7 +63,7 @@ void isLogging_warning(char *fmt, ...) {
 void isLogging_err(char *fmt, ...) {
   va_list arg_ptr;
 
-  va_start(arg_ptr, *fmt);
+  va_start(arg_ptr, fmt);
   vsyslog(LOG_ERR, fmt, arg_ptr);
   va_end(arg_ptr);
 }
@@ -79,7 +71,7 @@ void isLogging_err(char *fmt, ...) {
 void isLogging_crit(char *fmt, ...) {
   va_list arg_ptr;
 
-  va_start(arg_ptr, *fmt);
+  va_start(arg_ptr, fmt);
   vsyslog(LOG_CRIT, fmt, arg_ptr);
   va_end(arg_ptr);
 }
@@ -87,7 +79,7 @@ void isLogging_crit(char *fmt, ...) {
 void isLogging_alert(char *fmt, ...) {
   va_list arg_ptr;
 
-  va_start(arg_ptr, *fmt);
+  va_start(arg_ptr, fmt);
   vsyslog(LOG_ALERT, fmt, arg_ptr);
   va_end(arg_ptr);
 }
@@ -95,7 +87,7 @@ void isLogging_alert(char *fmt, ...) {
 void isLogging_emerg(char *fmt, ...) {
   va_list arg_ptr;
 
-  va_start(arg_ptr, *fmt);
+  va_start(arg_ptr, fmt);
   vsyslog(LOG_EMERG, fmt, arg_ptr);
   va_end(arg_ptr);
 }
