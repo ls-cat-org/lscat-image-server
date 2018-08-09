227f857e1307a825acf060d97e52e296dae4e446
Keith Brister <k-brister@northwestern.edu>
2018-08-09 15:32:05 -0500

    besthome not correctly added to path

--- is_indexing_setup.sh
+++ is_indexing_setup.sh
@@ -7,7 +7,7 @@ source /pf/opt/PHENIX/phenix_env.sh
 source /pf/opt/CCP4/setup-scripts/ccp4.setup-sh
 source /pf/opt/ARPWARP/arpwarp_setup.bash
 export besthome=/pf/opt/BEST
-export PATH=${PATH}:besthome
+export PATH=${PATH}:${besthome}
 source /usr/local/src/RAPD/rapd.shrc
 
 
