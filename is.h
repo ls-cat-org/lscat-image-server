ls-cat-org/lscat-image-server

commit 60da848bfa303aec7d48553e705ca12b655a3171

Add support for exporting metadata from Eiger 2 datasets to JSON for the web app.
This change replaces the old implementation for extracting HDF5 metadata altogether.

Incidentally, this change fixes several HDF5 descriptor leaks (similar to unix fd's)
and a memory leak, and it also adds support for metadata in the form of tensors of
order > 2.

is.h
@@ -7,6 +7,9 @@
 /** Enable reentrant version of hsearch that is more sensible in a
  ** multi-threaded world.
  */
+#ifndef IS_INCLUDE_H
+#define IS_INCLUDE_H
+
 #define _GNU_SOURCE
 
 #include <errno.h>
@@ -243,6 +246,15 @@ typedef struct isSubProcess_struct {
   int rtn;                              // return value of sub process
 } isSubProcess_type;
 
+/** h5 to json equivalencies.  We read HDF5 properties and convert
+ ** them to json to use and/or transmit back to the user's browser.
+ */
+struct h5_json_property {
+  const char *h5_location;  //!< HDF5 property name
+  const char *json_name;    //!< JSON equivalent
+};
+
+
 
 extern char *file_name_component(const char *parent_id, const char *path);
 extern double get_double_from_json_object(const char *cid,  const json_t *j, const char *key);
@@ -298,3 +310,41 @@ extern void set_json_object_real(const char *cid, json_t *j, const char *key, do
 extern void set_json_object_string(const char *cid, json_t *j, const char *key, const char *fmt, ...);
 extern zmq_pollitem_t *isGetZMQPollItems();
 extern zmq_pollitem_t *isRemakeZMQPollItems(void *parent_router, void *err_rep, void *err_dealer);
+
+// Get the software_version first so we can determine which params we have.
+extern const struct h5_json_property json_convert_software_version;
+
+/**
+ * Our mapping between hdf5 file properties and our metadata object properties for 
+ * datasets produced by the Eiger 2X 16M, and all other v1.8 DCUs.
+ */
+extern const struct h5_json_property json_convert_array_1_8[];
+extern const size_t json_convert_array_1_8_size;
+
+/**
+ * Our mapping between hdf5 file properties and our metadata object properties for 
+ * datasets produced by the Eiger 9M, and all other v1.6 DCUs.
+ */
+extern const struct h5_json_property json_convert_array_1_6[];
+extern const size_t json_convert_array_1_6_size;
+
+/**
+ * Converts an HDF5 Dataset (a single property within a .h5 file) into a JSON object.
+ * @param [in] file An HDF5 file handle created by H5Fopen.
+ * @param [in] property A mapping of an HDF5 dataset to a desired output JSON field name.
+ * @return A jansson library-internal representation of a JSON object. Returns NULL on failure.
+ */
+extern json_t* h5_property_to_json(hid_t file, const struct h5_json_property* property);
+
+/**
+ * Gets the Dectris DCU software version which produced the specified HDF5 archive (i.e. master file).
+ *
+ * @param [in] file An HDF5 archive (.h5 file), i.e. the "master file" containing image metadata.
+ * @return The software version of a Dectris detector DCU. Returns NULL on failure.
+ *
+ * @warning  { This function creates a string using malloc(), but caller is responsible for calling
+ *             free() when no longer needed. }
+ */
+extern json_t* get_dcu_version(hid_t file);
+
+#endif // header guard

isH5.c
@@ -90,27 +90,6 @@ h5_to_json_t json_convert_array[] = {
   { "/entry/instrument/detector/detectorSpecific/x_pixels_in_detector",            "x_pixels_in_detector",                            'i'},
   { "/entry/instrument/detector/y_pixel_size",                                     "y_pixel_size",                                    'f'},
   { "/entry/instrument/detector/detectorSpecific/y_pixels_in_detector",            "y_pixels_in_detector",                            'i'},
-
-  /* 
-     Params new to the Eiger2X 16M. If it cannot be described as a string or primitive type, or if it isn't in
-     the nexus format docs (https://manual.nexusformat.org/nxdl-types.html), it's commented out for now.     
-   */
-  {"/entry/definition", "definition", 's'},
-  /*{"/entry/instrument/detector/detectorSpecific/countrate_correction_lookuptable", "countrate_correction_lookuptable", 's'},*/
-  /*{"/entry/instrument/detector/detectorSpecific/countrate_correction_table", "countrate_correction_table", ''},*/
-  /*{"/entry/instrument/detector/detectorSpecific/distance", "distance", ''},*/
-  {"/entry/instrument/detector/detectorSpecific/module/data_origin", "data_origin", 'i'},
-  {"/entry/instrument/detector/detectorSpecific/module/data_size", "data_size", 'i'},
-  /*{"/entry/instrument/detector/detectorSpecific/module/fast_pixel_direction", "fast_pixel_direction", ''},*/
-  /*{"/entry/instrument/detector/detectorSpecific/module/module_index", "module_index", ''},*/
-  /*{"/entry/instrument/detector/detectorSpecific/module/module_offset", "module_offset", ''},*/
-  /*{"/entry/instrument/detector/detectorSpecific/module/slow_pixel_direction", "slow_pixel_direction", ''},*/
-  {"/entry/instrument/detector/detectorSpecific/saturation_value", "saturation_value", 'i'},
-  /*{"/entry/instrument/detector/detectorSpecific/transformations/translation", "translation", ''},*/
-  {"/entry/instrument/detector/detectorSpecific/type", "type", 's'},
-  {"/entry/instrument/detector/detectorSpecific/sample/beam/incident_wavelength", "incident_wavelength", 'f'},
-  /*{"/entry/instrument/detector/detectorSpecific/sample/goniometer", "goniometer", ''},*/
-  /*{"/entry/instrument/detector/detectorSpecific/sample/transformations", "transformations", ''}*/
 };
 
 /** Get a hdf5 property as a JSON object.
@@ -350,7 +329,12 @@ json_t *isH5GetMeta(isWorkerContext_t *wctx, const char *fn) {
   json_t *tmp_obj;              // temporary json object used to create meta
   int i;                        // loop over json conversion array
   int err;                      // error code for procedures that like to return ints.
-
+  
+  json_t* dcu_version;          // Eiger DCU software version
+  const struct h5_json_property* properties = NULL;
+  int n_properties = -1;
+  
+  
   //
   // Open up the master file
   //
@@ -375,9 +359,26 @@ json_t *isH5GetMeta(isWorkerContext_t *wctx, const char *fn) {
   }
 
   pthread_mutex_lock(&wctx->metaMutex);
-  for (i=0; i < sizeof(json_convert_array)/sizeof(json_convert_array[0]); i++) {
-    tmp_obj = get_json(fn, master_file, &json_convert_array[i]);
-
+  //
+  // Get the software version and the associated properties to convert.
+  //
+  dcu_version = get_dcu_version(master_file);
+  if (dcu_version == NULL) {
+    isLogging_err("%s: failed to get DCU version %s\n", id, fn);
+  }
+  
+  const char* dcu_version_str = json_string_value( json_object_get(dcu_version, json_convert_software_version.json_name) );
+  if (strcmp("1.8.0", dcu_version_str) == 0) {
+    properties = json_convert_array_1_8;
+    n_properties = json_convert_array_1_8_size;
+  } else {
+    properties = json_convert_array_1_6;
+    n_properties = json_convert_array_1_6_size;
+  }
+  
+  for (i=0; i < n_properties; i++) {
+    tmp_obj = h5_property_to_json(master_file, &properties[i]);
+    
     if (!tmp_obj) {
       continue; // Some variables were added/removed in newer versions, no problem.
     }

Makefile
@@ -1,4 +1,8 @@
-all: is
+
+CC=gcc
+CFLAGS=-g -Wall -I /usr/include/hdf5/serial -L /usr/lib/x86_64-linux-gnu/hdf5/serial -L /usr/local/lib64 -L /usr/local/lib -L/usr/lib
+
+all: is isConvertTest
 
 distclean:
 	@rm -f *.o is
@@ -20,46 +24,52 @@ install:
 	install --mode=644 is.logrotate /etc/logrotate.d/is
 
 isLogging.o: isLogging.c is.h Makefile
-	gcc -g -Wall -c isLogging.c
+	$(CC) $(CFLAGS) -c isLogging.c
+
+isConvert.o: isConvert.c is.h Makefile
+	$(CC) $(CFLAGS) -c isConvert.c
 
 isData.o: isData.c is.h Makefile
-	gcc -g -Wall -c isData.c
+	$(CC) $(CFLAGS) -c isData.c
 
 isWorker.o: isWorker.c is.h Makefile
-	gcc -g -Wall -c isWorker.c
+	$(CC) $(CFLAGS) -c isWorker.c
 
 isProcessManagement.o: isProcessManagement.c is.h Makefile
-	gcc -g -Wall -c isProcessManagement.c
+	$(CC) $(CFLAGS) -c isProcessManagement.c
 
 isUtilities.o: isUtilities.c is.h Makefile
-	gcc -g -Wall -c isUtilities.c 
+	$(CC) $(CFLAGS) -c isUtilities.c 
 
 isH5.o: isH5.c is.h Makefile
-	gcc -g -Wall -c isH5.c
+	$(CC) $(CFLAGS) -c isH5.c
 
 isRayonix.o: isRayonix.c is.h Makefile
-	gcc -g -Wall -c isRayonix.c
+	$(CC) $(CFLAGS) -c isRayonix.c
 
 isReduceImage.o: isReduceImage.c is.h Makefile
-	gcc -g -Wall -c isReduceImage.c
+	$(CC) $(CFLAGS) -c isReduceImage.c
 
 isJpeg.o: isJpeg.c is.h Makefile
-	gcc -g -Wall -c isJpeg.c
+	$(CC) $(CFLAGS) -c isJpeg.c
 
 isIndex.o: isIndex.c is.h Makefile
-	gcc -g -Wall -c isIndex.c
+	$(CC) $(CFLAGS) -c isIndex.c
 
 isRsync.o: isRsync.c is.h Makefile
-	gcc -g -Wall -c isRsync.c
+	$(CC) $(CFLAGS) -c isRsync.c
 
 isSpots.o: isSpots.c is.h Makefile
-	gcc -g -Wall -c isSpots.c
+	$(CC) $(CFLAGS) -c isSpots.c
 
 isBitmapFont.o: isBitmapFont.c is.h Makefile
-	gcc -g -Wall -c isBitmapFont.c
+	$(CC) $(CFLAGS) -c isBitmapFont.c
 
 isSubProcess.o: isSubProcess.c is.h Makefile
-	gcc -g -Wall -c isSubProcess.c
+	$(CC) $(CFLAGS) -c isSubProcess.c
 
-is: isMain.c is.h Makefile isUtilities.o isH5.o isRayonix.o isProcessManagement.o isWorker.o isData.o isReduceImage.o isJpeg.o isBitmapFont.o isIndex.o isSpots.o isRsync.o isLogging.o isSubProcess.o
-	gcc -g -Wall isMain.c -L /usr/local/lib64 -L /usr/local/lib -L/usr/lib -o is isLogging.o isUtilities.o isH5.o isRayonix.o isProcessManagement.o isWorker.o isSubProcess.o isData.o isReduceImage.o isJpeg.o isIndex.o isSpots.o isRsync.o isBitmapFont.o -lhiredis -ljansson -lhdf5 -ltiff -lcrypto -ljpeg -lm -lzmq -pthread
+isConvertTest: isConvertTest.c is.h Makefile isConvert.o isUtilities.o isH5.o isRayonix.o isProcessManagement.o isWorker.o isData.o isReduceImage.o isJpeg.o isBitmapFont.o isIndex.o isSpots.o isRsync.o isLogging.o isSubProcess.o
+	$(CC) $(CFLAGS) isConvertTest.c -o isConvertTest isLogging.o isConvert.o isUtilities.o isH5.o isRayonix.o isProcessManagement.o isWorker.o isSubProcess.o isData.o isReduceImage.o isJpeg.o isIndex.o isSpots.o isRsync.o isBitmapFont.o -lhiredis -ljansson -lhdf5 -ltiff -lcrypto -ljpeg -lm -lzmq -pthread
+
+is: isMain.c is.h Makefile isConvert.o isUtilities.o isH5.o isRayonix.o isProcessManagement.o isWorker.o isData.o isReduceImage.o isJpeg.o isBitmapFont.o isIndex.o isSpots.o isRsync.o isLogging.o isSubProcess.o
+	$(CC) $(CFLAGS) isMain.c -o is isLogging.o isConvert.o isUtilities.o isH5.o isRayonix.o isProcessManagement.o isWorker.o isSubProcess.o isData.o isReduceImage.o isJpeg.o isIndex.o isSpots.o isRsync.o isBitmapFont.o -lhiredis -ljansson -lhdf5 -ltiff -lcrypto -ljpeg -lm -lzmq -pthread

isBitmapFont.h
@@ -1,3 +1,5 @@
+#ifndef IS_BITMAP_FONT_H
+#define IS_BITMAP_FONT_H
 /** @file isBitmapFont.h
  ** @copyright 2014-2016 by Northwestern University
  ** @author Keith Brister
@@ -281,3 +283,4 @@ extern const int n_isBitmapFontBitmaps;
 
 /** @endcond
  */
+#endif // header guard

isConvert.h
@@ -1,0 +1,50 @@
+#ifndef IS_CONVERT_H
+#define IS_CONVERT_H
+
+#include <jansson.h>
+#include <hdf5.h>
+
+#include "is.h"
+
+/** h5 to json equivalencies.  We read HDF5 properties and convert
+ ** them to json to use and/or transmit back to the user's browser.
+ */
+struct h5_json_property {
+  const char *h5_location;  //!< HDF5 property name
+  const char *json_name;    //!< JSON equivalent
+};
+
+// Get the software_version first so we can determine which params we have.
+extern const struct h5_json_property json_convert_software_version;
+
+/**
+ * Our mapping between hdf5 file properties and our metadata object properties for 
+ * datasets produced by the Eiger 2X 16M, and all other v1.8 DCUs.
+ */
+extern const struct h5_json_property json_convert_array_1_8[];
+
+/**
+ * Our mapping between hdf5 file properties and our metadata object properties for 
+ * datasets produced by the Eiger 9M, and all other v1.6 DCUs.
+ */
+extern const struct h5_json_property json_convert_array_1_6[];
+
+/**
+ * Converts an HDF5 Dataset (a single property within a .h5 file) into a JSON object.
+ * @param [in] file An HDF5 file handle created by H5Fopen.
+ * @param [in] property A mapping of an HDF5 dataset to a desired output JSON field name.
+ * @return A jansson library-internal representation of a JSON object. Returns NULL on failure.
+ */
+extern json_t* h5_property_to_json(hid_t file, const struct h5_json_property* property);
+
+/**
+ * Gets the Dectris DCU software version which produced the specified HDF5 archive (i.e. master file).
+ *
+ * @param [in] file An HDF5 archive (.h5 file), i.e. the "master file" containing image metadata.
+ * @return The software version of a Dectris detector DCU. Returns NULL on failure.
+ *
+ * @warning  { This function creates a string using malloc(), but caller is responsible for calling
+ *             free() when no longer needed. }
+ */
+extern json_t* get_dcu_version(hid_t file);
+#endif // Header guard

isConvertTest.c
@@ -1,0 +1,64 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <hdf5.h>
+#include <jansson.h>
+
+#include "is.h"
+
+void usage() {
+  fprintf(stderr, "isConvertTest <h5_file>\n\n");
+}
+
+/**
+ * Usage: isConvertTest <h5_file>
+ */
+int main(int argc, char** argv) {
+  if (argc != 2) {
+    usage();
+    return 1;
+  }
+
+  //
+  // Open up the master file
+  //
+  hid_t master_file = H5Fopen(argv[1], H5F_ACC_RDONLY, H5P_DEFAULT);
+  if (master_file < 0) {
+    fprintf(stderr, "failed to open master file\n");
+    return 1;
+  }
+
+  json_t* dcu_version = get_dcu_version(master_file);
+  if (dcu_version == NULL) {
+    fprintf(stderr, "failed to get DCU version\n");
+  }
+  
+  char* dcu_version_str = json_string_value( json_object_get(dcu_version, json_convert_software_version.json_name) );
+  if (strcmp("1.8.0", dcu_version_str) == 0) {
+    printf("Testing 1.8 compatibility\n\n");
+    for (int i=0; i < json_convert_array_1_8_size; i++) {
+      json_t* tmp_obj = h5_property_to_json(master_file, &json_convert_array_1_8[i]);
+      if (tmp_obj != NULL) {
+	printf("\n%s\n", json_dumps(tmp_obj, JSON_INDENT(2)));
+      } else {
+	printf("\nFAILED: %s\n", json_convert_array_1_8[i].h5_location);
+      }
+      json_decref(tmp_obj);
+    }
+    printf("\n\n");
+  } else {
+    printf("Testing 1.6 compatibility\n\n");
+    for (int i=0; i < json_convert_array_1_6_size; i++) {
+      json_t* tmp_obj = h5_property_to_json(master_file, &json_convert_array_1_6[i]);
+      if (tmp_obj != NULL) {
+	printf("\n%s\n", json_dumps(tmp_obj, JSON_INDENT(2)));
+      } else {
+	printf("\nFAILED: %s\n", json_convert_array_1_6[i].h5_location);
+      }
+      json_decref(tmp_obj);
+    }
+    printf("\n\n");
+  }
+
+  H5Fclose(master_file);
+  return 0;
+}

commit 3295915f75a5dcbbff98de04852059405b274e51

Add support for indexing images produced by LS-CAT's Eiger2X 16M setup on station D.
Also fixed a memory leak introduced by commit 60da848 in isH5.c.

Also added a NULL check in case an HDF5 archive to prevent a segfault,
should an H5 file not have the software_version attribute.

Also updated a couple unrelated code comments.

is.h
@@ -345,4 +345,20 @@ extern json_t* h5_property_to_json(hid_t file, const struct h5_json_property* pr
  */
 extern json_t* get_dcu_version(hid_t file);
 
+/**
+   Gets the DCU version.
+
+   @param [in] master_file The filepath of an HDF5 archive.
+   @param [in,out] strbuf A preallocated string buffer.
+   @param [in] strbuf_size The size of the string buffer in bytes.
+   @return { The value for the "software_version" attribute found in an HDF5 archive 
+   produced by an Eiger or Eiger2 DCU. }
+
+   @precondition strbuf is not NULL, strbuf_size > 0
+
+   @note { This function is a hack to support indexing Eiger2 image sets, and all uses 
+   of it are used to infer which of two detectors are used on-site. }
+ */
+extern void get_dcu_version_str(const char* master_file, char* strbuf, size_t strbuf_size);
+
 #endif // header guard

isConvert.c
@@ -581,3 +581,47 @@ json_t* h5_property_to_json(hid_t file, const struct h5_json_property* property)
 json_t* get_dcu_version(hid_t file) {
   return h5_property_to_json(file, &json_convert_software_version);
 }
+
+// This hack gets the DCU software version as a string so that isIndexImages in
+// isIndexImages.c can infer which detector made the image(s).
+void get_dcu_version_str(const char* master_file, char* strbuf, size_t strbuf_size) {
+#define ERRLOG(msg) isLogging_err("%s: %s - " msg "\n", log_id, master_file);
+  static const char* log_id = FILEID "get_dcu_version_str";
+
+  hid_t master_file_id = -1;
+  json_t* json_obj     = NULL;
+  const char* value    = NULL;
+
+  if (strbuf == NULL && strbuf_size <= 0) {
+    ERRLOG("bad arguments, strbuf must be non-NULL and nonzero size");
+    goto error;
+  }
+  strbuf[0] = '\0';
+  
+  master_file_id = H5Fopen(master_file, H5F_ACC_RDONLY, H5P_DEFAULT);
+  if (master_file < 0) {
+    ERRLOG("failed to open master file");
+    goto error;
+  }
+
+  json_obj = get_dcu_version(master_file_id);
+  if (json_obj == NULL) {
+    ERRLOG("failed to read/parse DCU version");
+    goto error;
+  }
+  
+  value = json_string_value( json_object_get(json_obj, json_convert_software_version.json_name) );
+  if (value != NULL) {
+    strncpy(strbuf, value, strbuf_size);
+  }
+  
+  json_decref(json_obj);
+  H5Fclose(master_file_id);
+  return;
+  
+ error:
+  if (json_obj != NULL) json_decref(json_obj);
+  if (master_file_id >= 0) H5Fclose(master_file_id);
+  return;
+#undef ERRLOG
+}

isH5.c
@@ -330,7 +330,7 @@ json_t *isH5GetMeta(isWorkerContext_t *wctx, const char *fn) {
   int i;                        // loop over json conversion array
   int err;                      // error code for procedures that like to return ints.
   
-  json_t* dcu_version;          // Eiger DCU software version
+  json_t* dcu_version = NULL;   // Eiger DCU software version
   const struct h5_json_property* properties = NULL;
   int n_properties = -1;
   
@@ -368,12 +368,17 @@ json_t *isH5GetMeta(isWorkerContext_t *wctx, const char *fn) {
   }
   
   const char* dcu_version_str = json_string_value( json_object_get(dcu_version, json_convert_software_version.json_name) );
-  if (strcmp("1.8.0", dcu_version_str) == 0) {
+  if (dcu_version_str != NULL && strcmp("1.8.0", dcu_version_str) == 0) {
     properties = json_convert_array_1_8;
     n_properties = json_convert_array_1_8_size;
   } else {
     properties = json_convert_array_1_6;
     n_properties = json_convert_array_1_6_size;
+  }
+  
+  if (dcu_version != NULL) {
+    json_decref(dcu_version); // also frees the memory used by dcu_version_str
+    dcu_version = NULL;
   }
   
   for (i=0; i < n_properties; i++) {

isIndex.c
@@ -40,7 +40,18 @@ json_t *isIndexImages(redisContext *rrc, const char *progressPublisher, const ch
   struct pollfd polllist[5];    // list of fd's we're polling
   struct pollfd pollout;        // poll for stdout
   struct pollfd pollprogress;   // poll for progress
+  char   dcu_version[16];       // Eiger detector software version, if applicable.
+  char*  detector_arg;          // Optional
 
+  // Infer the detector setup that produced the HDF5-based image set
+  // based on the DCU software version. This is specific to LS-CAT's
+  // setup as of Jan 2023-Apr 2023, but that's ok. There are only 2
+  // detectors in our lab, and both have used and will continue to use
+  // the same software versions they always have until image server is
+  // abandoned.
+  get_dcu_version_str(f1, dcu_version, sizeof(dcu_version));
+  detector_arg = (strcmp(dcu_version, "1.8.0") == 0) ?
+    "--detector lscat_dectris_eiger2_16m" : " ";
 
   rtn = NULL;
 
@@ -104,10 +115,12 @@ json_t *isIndexImages(redisContext *rrc, const char *progressPublisher, const ch
     }
 
     //
-    // Make hard links in tmp directory to data files
+    // Make symlinks in tmp directory to data files in lustre.
     //
-    // (Hardlinks aren't working to /pf/esafs but are from
-    // /pf/esafs-bu, changed to soft links for now.)
+    // Note: Because both locations are in the "same" filesystem, normally we could
+    // use hard links. However, hard links on lustre have given us problems, likely
+    // because it's not a local filesystem, and it's also a filesystem on top of a
+    // filesystem, i.e. lustre -> ext4.
     //
     f1_local = NULL;
     if (f1 && strlen(f1)) {
@@ -135,7 +148,8 @@ json_t *isIndexImages(redisContext *rrc, const char *progressPublisher, const ch
       exit (-1);
     }
     fprintf(shell_script, "#! /bin/bash\n");
-    fprintf(shell_script, "rapd.index --json --json-fd %d --progress-fd %d ", pipejson[1], pipeprogress[1]);
+    fprintf(shell_script, "rapd.index %s --json --json-fd %d --progress-fd %d ",
+	    detector_arg, pipejson[1], pipeprogress[1]);
     fflush(shell_script);
 
     // Always need f1 but not always f2